b77a8bc8334d9ef82f21f7325849e28abea0d4ed
Biruk-N <86155266+Biruk-N@users.noreply.github.com>
2021-08-17 23:29:00 +0300

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    echo "<a href = '/default.asp'> home </a>
+    <a href = '/default.asp'> Html Tutorial </a>
+    <a href = '/default.asp'> Css Tutorial </a>
+    <a href = '/default.asp'> Php Tutorial </a>
+    <a href = '/default.asp'> Js Tutorial </a>
+
+    
+    " ;
+    ?>
+
+    
+</body>
+</html>
